f211d6c84435148cd10b2d6de6a21d586d36cfbc
brunowang <625653507@qq.com>
2023-09-04 11:51:28 +0800

    add description of leetcode problem

--- leetcode-go/linkedlist/leetcode21.go
+++ leetcode-go/linkedlist/leetcode21.go
@@ -1,5 +1,8 @@
 package linkedlist
 
+/*
+将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+*/
 func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
 	head := &ListNode{}
 	p, p1, p2 := head, list1, list2
