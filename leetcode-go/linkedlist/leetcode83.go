b083f92f23dfc554f4dd858a38de52c3f14ecd11
brunowang <625653507@qq.com>
2023-09-07 13:09:07 +0800

    practice delete duplicates value from linked-list

---
+++ leetcode-go/linkedlist/leetcode83.go
@@ -0,0 +1,21 @@
+package linkedlist
+
+/*
+给定一个已排序的链表的头 head ， 删除所有重复的元素，使每个元素只出现一次 。返回 已排序的链表 。
+*/
+func deleteDuplicates(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+
+	slow, fast := head, head
+	for fast != nil {
+		if fast.Val != slow.Val {
+			slow.Next = fast
+			slow = slow.Next
+		}
+		fast = fast.Next
+	}
+	slow.Next = nil
+	return head
+}
---
+++ leetcode-go/linkedlist/leetcode83_test.go
@@ -0,0 +1,27 @@
+package linkedlist
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_deleteDuplicates(t *testing.T) {
+	type args struct {
+		head *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want *ListNode
+	}{
+		{"test", args{NewList(1, 1, 2)}, NewList(1, 2)},
+		{"test", args{NewList(1, 1, 2, 3, 3)}, NewList(1, 2, 3)},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := deleteDuplicates(tt.args.head); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("deleteDuplicates() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
