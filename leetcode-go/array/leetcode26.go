5ee1452805f194fe90b627279531ea494a65d0ff
brunowang <625653507@qq.com>
2023-09-10 10:25:48 +0800

    practice algorithm on array

---
+++ leetcode-go/array/leetcode26.go
@@ -0,0 +1,19 @@
+package array
+
+/*
+给你一个 升序排列 的数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。元素的 相对顺序 应该保持 一致 。然后返回 nums 中唯一元素的个数。
+*/
+func removeDuplicates(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	slow, fast := 0, 0
+	for fast < len(nums) {
+		if nums[fast] != nums[slow] {
+			slow++
+			nums[slow] = nums[fast]
+		}
+		fast++
+	}
+	return slow + 1
+}
