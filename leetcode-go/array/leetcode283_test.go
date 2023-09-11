19b2a9508d8dc9edee1820eecb6a11d6b5625059
brunowang <625653507@qq.com>
2023-09-11 15:57:43 +0800

    practice algorithm on array

---
+++ leetcode-go/array/leetcode283_test.go
@@ -0,0 +1,27 @@
+package array
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_moveZeroes(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{"test", args{[]int{0, 1, 0, 3, 12}}, []int{1, 3, 12, 0, 0}},
+		{"test", args{[]int{0}}, []int{0}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := moveZeroes(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("moveZeroes() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
