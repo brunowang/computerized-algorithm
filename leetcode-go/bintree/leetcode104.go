562fb8b024bbdeaf2f42bc01810b404f39f49ebc
brunowang <625653507@qq.com>
2023-09-15 19:25:23 +0800

    practice binary tree problem

---
+++ leetcode-go/bintree/leetcode104.go
@@ -0,0 +1,25 @@
+package bintree
+
+/*
+给定一个二叉树 root ，返回其最大深度。
+二叉树的 最大深度 是指从根节点到最远叶子节点的最长路径上的节点数。
+*/
+func maxDepth(root *TreeNode) int {
+	return dp(root)
+}
+
+func dp(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	lmax := dp(root.Left)
+	rmax := dp(root.Right)
+	return max(lmax, rmax) + 1
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
